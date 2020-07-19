eb198b5982811b72c5e7e7d1be338cf77b1f3dba
Luke Evans <lukeevanstax@gmail.com>
2020-07-18 23:27:37 -0400

    adding in initial files for readme project

---
+++ index.js
@@ -0,0 +1,20 @@
+// installs
+const inquirer = require("")
+
+// array of questions for user
+const questions = [
+fswritefilesync
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generateMarkdown(data));
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
